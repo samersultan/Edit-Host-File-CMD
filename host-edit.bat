35087d93e4dc23a9242e81ab739859ed6f364db3
Samer Sultan <samersultan1@gmail.com>
2019-03-25 09:29:21 -0500

    Update host-edit.bat

--- host-edit.bat
+++ host-edit.bat
@@ -1,6 +1,6 @@
 @echo off
 rem ********************************************************
-rem ******* Replace xxx with IP and URL.com with URL  ******
+rem ******* Replace xxx with IP / xxx.com with URL  ******
 rem ********************************************************
 
 echo xxx.xxx.xxx.xxx    URL.COM >> %WINDIR%\System32\Drivers\Etc\Hosts
